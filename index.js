4cfbaa5c37df07e5650865f146ec780cdd4cc454
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-29 18:32:53 -0500

    super extends etc

--- index.js
+++ index.js
@@ -69,3 +69,65 @@ thereCar.drive();
 hisCar.drive();
 herCar.drive();
 jimmyCar.drive();
+
+
+class User {
+    constructor(username, email, firstName, lastName, isActive, password) {
+        this.username = username || "guest";
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isActive = isActive || false;
+        this.loginAttempts = 0;
+        this.password = password;
+    }
+
+    login(password) {
+        if (this.password === password) {
+            this.isActive = true;
+            this.loginAttempts = 0;
+            console.log(`Welcome back, ${this.firstName}!`);
+        } else {
+            this.loginAttempts++;
+            console.log("Incorrect password. Please try again.");
+        }
+
+        if (this.loginAttempts >= 3) {
+            console.log("Account locked due to too many failed login attempts.");
+            return;
+        }
+
+        return true;
+    }
+}
+
+let basicUser = new User("superdave", "superdave@email.com", "dave", "superwinzki", true, "password123");
+
+console.log(basicUser);
+basicUser.login("password321");
+console.log(basicUser);
+basicUser.login("password1");
+console.log(basicUser);
+basicUser.login("password123");
+
+
+class Student extends User {
+    constructor(username, email, firstName, lastName, isActive, password, studentId, major, gpa, credits, attendance) {
+        super(username, email, firstName, lastName, isActive, password);
+        this.studentId = studentId || "unknown";
+        this.major = major || "undeclared";
+        this.gpa = gpa || 0.0;
+        this.credits = credits || 0;
+        this.attendance = attendance;
+    }
+
+    study() {
+        console.log(`${this.firstName} is studying!`);
+    }
+}
+
+let meAsAStudent = new Student("superchris", "superchris@email.com", "chris", "superless", false, "password123", "123456", "Sociology", 3.5, 30, 95);
+console.log(meAsAStudent);
+meAsAStudent.login("password123");
+meAsAStudent.study();
+
