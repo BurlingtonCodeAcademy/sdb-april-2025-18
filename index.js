43770c5328088a1739484ad49ff4abaf4974a07b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-29 17:39:27 -0500

    class example

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("HAPPY THURSDAY!")
+// creating the Pizza class
+class Pizza {
+    constructor(size, toppings) {
+        this.size = size;
+        this.toppings = toppings;
+    }
+
+    describe() {
+        return `This is a ${this.size} pizza with ${this.toppings} on top.`
+    }
+}
+
+let ourPizza = new Pizza();
+
+// setting the size and toppings
+ourPizza.size = "large";
+ourPizza.toppings = "pepperoni and mushrooms";
+
+// console log the description of the pizza
+console.log(ourPizza.describe())
